8ffb47e2bab938ed49bdfc01b13c76509a29e0b4
Алексей <alexy.74@mail.ru>
2024-06-27 16:28:29 +0300

    переводы

--- app/Models/Slider.php
+++ app/Models/Slider.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use TCG\Voyager\Traits\Resizable;
 use TCG\Voyager\Traits\Translatable;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
 
-class Slider extends Model
+class Slider extends Model implements HasMedia
 {
+    use InteractsWithMedia;
     use Resizable;
     use Translatable;
     protected $translatable = ['name', 'description'];
--- resources/views/partials/main-slider.blade.php
+++ resources/views/partials/main-slider.blade.php
@@ -5,12 +5,13 @@
             <div class="main-slider_slider">
                 @foreach($mainslider as $slid)
                 <div class="main-slider_slider-item">
-                    <img src="{{ Voyager::image( $slid->img ) }}" alt="">
+                    {{$slid->getFirstMedia('img') }}
+{{--                    <img src="{{ Voyager::image( $slid->img ) }}" alt="">--}}
                     <div class="info">
                         <div class="info-name"><h2 class="like-h1">{!! $slid->name !!}</h2></div>
                         <div class="info-text">{{$slid->description}}</div>
-                        <a href="{{$slid->link1}}" class="btn btn-orange">В каталог</a>
-                        <a href="{{$slid->link2}}" class="btn btn-transparent">к коллекции</a>
+                        <a href="{{$slid->link1}}" class="btn btn-orange">@lang('text.to-catalog')</a>
+                        <a href="{{$slid->link2}}" class="btn btn-transparent">@lang('text.to-collection')</a>
                     </div>
                 </div>
                 @endforeach
